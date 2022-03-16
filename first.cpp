831b9750cf2b0edd361759921fa854f8f9bbbe6a
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-03-16 14:03:04 +0530

    Added comments

--- first.cpp
+++ first.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout<<"Hello World";
+	cout<<"Hello World";
 	int a=10, b=20;
-cout << "Sum : " << a+b;
+	cout << "Sum : " << a+b; // Sum of two numbers.
 }
