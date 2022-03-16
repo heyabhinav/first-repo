e960ab508bf3626864c503c17677d49223313c74
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-03-16 14:06:16 +0530

    Swapped values if variables a and b at line 9.

--- first.cpp
+++ first.cpp
@@ -4,4 +4,10 @@ int main(){
 	cout<<"Hello World";
 	int a=10, b=20;
 	cout << "Sum : " << a+b; // Sum of two numbers.
+	
+	//Swapping
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	cout << "Values after swapping -> " << "a : " << a << "b : " << b;
 }
